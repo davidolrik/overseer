e9c1b5c66fe7547a0a57950e4f9cabff65cf2d5c
David Jack Wange Olrik <david@olrik.dk>
2025-10-16 12:08:03 +0200

    feat: Show daemon command in docs

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -8,8 +8,14 @@ import (
 func NewDaemonCommand() *cobra.Command {
 	daemonCmd := &cobra.Command{
 		Use:     "daemon",
-		Aliases: []string{},
-		Hidden:  true,
+		Aliases: []string{"server"},
+		Short:   "Run daemon in foreground (only for debugging)",
+		Long: `Run daemon in foreground (only for debugging)
+
+The daemon will be started automatically, so you rarely have to call this directly.
+
+If you need to debug a connection, or just want to have the daemon running in the
+foreground use this command.`,
 		Run: func(cmd *cobra.Command, args []string) {
 			d := daemon.New()
 			d.Run()
